fcaee3b5c155d53049a3cba3630ac847c1056b6d
chico <francisco.rivera.valverde@gmail.com>
2020-05-29 13:37:59 +0200

    fix

--- autosklearn/util/common.py
+++ autosklearn/util/common.py
@@ -7,6 +7,12 @@ from resource import *
 from tabulate import tabulate
 
 from memory_profiler import profile
+from datetime import datetime
+import sys
+import os
+import resource
+import re
+import psutil
 
 __all__ = [
     'check_pid',
@@ -15,8 +21,32 @@ __all__ = [
 
 
 def print_getrusage(message):
+
+    #RLIMIT_AS. The maximum size of the process's virtual memory (address space) in bytes.
+
+
+    #RSS is the Resident Set Size and is used to show how much memory is allocated to that process and is in RAM. It does not include memory that is swapped out. It does include memory from shared libraries as long as the pages from those libraries are actually in memory. It does include all stack and heap memory.
+    #
+    #VSZ is the Virtual Memory Size. It includes all memory that the process can access, including memory that is swapped out, memory that is allocated, but not used, and memory that is from shared libraries.
+    #
+    #So if process A has a 500K binary and is linked to 2500K of shared libraries, has 200K of stack/heap allocations of which 100K is actually in memory (rest is swapped or unused), and it has only actually loaded 1000K of the shared libraries and 400K of its own binary then:
+    #
+    #RSS: 400K + 1000K + 100K = 1500K
+    #VSZ: 500K + 2500K + 200K = 3200K
+
     print("\nLocation: {}".format(message))
-    for member in [RUSAGE_SELF, RUSAGE_CHILDREN]:
+    #Getting virtual memory size
+    pid = os.getpid()
+    with open(os.path.join("/proc", str(pid), "status")) as f:
+        lines = f.readlines()
+    _vmsize = [l for l in lines if l.startswith("VmSize")][0]
+    vmsize = int(_vmsize.split()[1])
+    print(f"vmsize={vmsize}")
+
+    for key, value in dict(psutil.virtual_memory()._asdict()).items():
+        print(f"psutil {key} = {value}")
+
+    for member in [RUSAGE_SELF, RUSAGE_CHILDREN, RUSAGE_THREAD]:
         print(f"member={member}")
         columns = ["Index", "Field", "Resource"]
         fields = [
