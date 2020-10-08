bbcfec425ddc236a93fc03eea42eb7856ba9a967
chico <francisco.rivera.valverde@gmail.com>
2020-10-08 20:35:47 +0200

    requirements fix

--- autosklearn/__init__.py
+++ autosklearn/__init__.py
@@ -7,11 +7,6 @@ from autosklearn.util import dependencies
 from autosklearn.__version__ import __version__  # noqa (imported but unused)
 
 
-requirements = pkg_resources.resource_string('autosklearn', 'requirements.txt')
-requirements = requirements.decode('utf-8')
-
-dependencies.verify_packages(requirements)
-
 if os.name != 'posix':
     raise ValueError(
         'Detected unsupported operating system: %s. Please check '
